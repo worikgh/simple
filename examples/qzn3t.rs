7d755a71f0280ab54f368eee0ce2639137b05ad6
Worik <5387413+worikgh@users.noreply.github.com>
2025-12-24 16:16:17 +1300

    New example

--- examples/qzn3t.rs
+++ examples/qzn3t.rs
@@ -6,6 +6,11 @@
  * to add a new rectangle.
  *
  * Added features:
+ * Rotate the screen 90 degrees counter clockwise
+ * Gravity effects squares to illustrate where "down" is
+ * Squares that hit "left side" (x == 0) wrap around, squares that hit
+ * "right side" (x == w) bounce (revrse direction) illustrating where
+ * left side is
  */
 
 extern crate rand;
@@ -32,32 +37,35 @@ struct Square {
     speed_x: f32,
     speed_y: f32,
     color: (u8, u8, u8, u8),
-    screen_width: u32,
-    screen_height: u32,
+    model_width: u32,
+    model_height: u32,
     width: u32,
     height: u32,
+    trend_left: bool,
 }
 
 impl Square {
     /// Generate a Square with random speed and color starting at the point you specify
-    fn new_at_position(x: f32, y: f32, screen_width: u32, screen_height: u32) -> Self {
+    fn new_at_position(x: f32, y: f32, model_width: u32, model_height: u32) -> Self {
+        //  FIXME!!  x, y, screen_width and screen_height should all be same type
         // generate a random angle and then use that angle to calculate the initial speed_x and
         // speed_y. We do this because it simplifies bouncing logic later in the update function.
         // The multiplication here is because random::<f32> appears to generate a value between 0
         // and 1, so we have to expand that range to [0, 2*PI] to get a full distribution of
         // possible angles.
         let angle: f32 = rand_up_to(f32::consts::PI * 2.0);
-
+        let trend_left = random::<u8>().is_multiple_of(2);
         Square {
             x,
             y,
             speed_x: angle.sin() * 8.0,
             speed_y: angle.cos() * 8.0,
             color: (random(), random(), random(), 255), // color is totally random
-            screen_width,
-            screen_height,
+            model_width,
+            model_height,
             width: 64,
             height: 64,
+            trend_left, //: random::<u8>().is_multiple_of(2),
         }
     }
 
@@ -74,89 +82,124 @@ impl Square {
     /// Move the Square the distance it needs to travel for one frame.
     /// The speed slows down, and gravity effects it too
     fn update(&mut self) {
+        // let start = format!(
+        //     "@({:0.0},{:0.0}) dX:{:0.1} dY:{:0.1}  LEFT:{}",
+        //     self.x, self.y, self.speed_x, self.speed_y, self.trend_left
+        // );
         self.x += self.speed_x;
         self.y += self.speed_y;
-        if self.x < 0.0 || self.x > self.screen_width as f32 {
-            self.speed_x *= -1f32;
+        if self.x < 0.0 {
+            // Hit "left side". Wrap around
+            self.x = self.model_width as f32;
+            if self.trend_left {
+                // Slow it down
+                self.speed_x *= 0.5;
+            }
+        } else if self.x > self.model_width as f32 {
+            // Hit "right side"  Make veloicity leftwards
+            self.speed_x = -self.speed_x.abs();
+            if self.speed_x.abs() < 2.0 && !self.trend_left {
+                self.speed_x = -rand_up_to(self.model_width as f32);
+            }
         }
-        // if self.y < 0.0 || self.y > self.screen_height as f32 {
-        //     self.speed_y *= -1f32;
-        // }
+
+        if self.y > self.model_height as f32 {
+            self.speed_y = -self.speed_y.abs();
+            if self.speed_y.abs() < 2.0 {
+                // fire from a cannon
+                self.speed_y = -rand_up_to(self.model_height as f32);
+            }
+        }
+
+        if self.y <= self.height as f32 / 2.0 {
+            // Hit roof.  Up is lower Y
+            self.y = self.height as f32 / 2.0;
+            // self.speed_y = rand_up_to(self.model_height as f32);
+            self.speed_y = rand_up_to(40.0);
+        }
 
         // Slow down
         self.speed_x *= 0.99;
         self.speed_y *= 0.99;
 
-        // Acceleration due to gravity.
-        self.speed_y -= 1.0;
-        if self.y <= self.height as f32 / 2.0 {
-            // eprintln!("Stopping: {self:?}");
-            // // Bounce
+        // Acceleration due to gravity.  Down is to higher y
+        self.speed_y += 1.0;
 
-            // Stop
-            self.speed_y = 0.0;
-            self.speed_x = 0.0;
-            self.y = self.height as f32 / 2.0;
-            self.speed_y = rand_up_to(40.0);
-            self.speed_x = rand_up_to(20.0) - 10.0;
+        if self.speed_y.abs() <= f32::EPSILON {
+            eprintln!("Speed_y is zero");
         }
-
-        if self.speed_y <= f32::EPSILON {}
+        // Drift....
+        self.speed_x += if self.trend_left { -0.1 } else { 0.1 };
+        // let end = format!(
+        //     "@({:0.0},{:0.0}) dX:{:0.1} dY:{:0.1}",
+        //     self.x, self.y, self.speed_x, self.speed_y,
+        // );
+        // eprintln!(
+        //     "{start} -> {end} {}x{}",
+        //     self.model_width, self.model_height
+        // );
     }
 
     /// Blit a square representing this object onto the Window.
     fn draw(&self, app: &mut Window) {
-        app.set_color(self.color.0, self.color.1, self.color.2, self.color.3);
-        // eprintln!("Draw: {self:?}");
-
-        let sw = self.screen_width;
-        let sh = self.screen_height;
-        let y_p = self.y / sh as f32;
-        let x_p = self.x / sw as f32;
-        if !(0.0..1.0).contains(&x_p) {
-            eprintln!("x_p: {x_p:0.3}");
+        {
+            let r = Rect::new(self.x as i32, self.y as i32, self.width, self.height);
+            fill_rect(app, r, self.color.into());
         }
-        if !(0.0..1.0).contains(&y_p) {
-            eprintln!("y_p: {y_p:0.3}");
+        {
+            let ind_rect = if self.trend_left {
+                Rect::new(
+                    self.x as i32,
+                    self.y as i32 - self.height as i32 / 4,
+                    self.width / 2,
+                    self.height / 4,
+                )
+            } else {
+                Rect::new(
+                    (self.x as u32 + self.width / 2) as i32,
+                    self.y as i32 - self.height as i32 / 2,
+                    self.width / 2,
+                    self.height / 4,
+                )
+            };
+
+            fill_rect(app, ind_rect, [255, 255, 255, 127]);
         }
-
-        let x = sw as f32 * y_p;
-        let y = sh as f32 - sh as f32 * x_p;
-
-        if !(0.0..sh as f32).contains(&x) {
-            eprintln!("Width: {sw} x: {x}");
-        }
-        if !(0.0..sh as f32).contains(&y) {
-            eprintln!("Height: {sh} y: {y}");
-        }
-
-        // let x = sw as f32 - self.x;
-        // let y = sh as f32 - self.y;
-        let draw_rect = Rect::new(
-            x as i32 - self.width as i32 / 2,
-            y as i32 - self.height as i32 / 2,
-            self.width,
-            self.height,
-        );
-
-        let _ = app.fill_rect(draw_rect);
     }
 }
 
+/// This is where the transformation of where drawing happens on
+/// screen is implemented.  Rotated anti-clockwise ninety degrees
+fn fill_rect(app: &mut Window, rect: Rect, colour: [u8; 4]) {
+    let ds = app.drawable_size();
+    let view_w = ds.0;
+    let view_h = ds.1;
+    let (x, y) = Window::translate_model_90c(rect.x(), rect.y(), view_w, view_h);
+
+    let draw_rect = Rect::new(x, y, rect.height(), rect.width());
+    eprintln!("fill_rect: {rect:?} -> {draw_rect:?}  {view_w}x{view_h}");
+    app.set_color(colour[0], colour[1], colour[2], colour[3]);
+    app.fill_rect(draw_rect);
+}
 fn main() {
     // Create an application
 
     // Get screen dimensions
     let (max_w, max_h) = Window::get_max_wh().unwrap();
-    let (max_w, max_h) = (max_w / 2, max_h / 2);
     let mut app = Window::new("Squares", max_w as u16, max_h as u16);
-    let (w, h) = app.drawable_size();
-    // Create some objects to live in the application
-    // let mut squares = vec![Square::new(w, h), Square::new(w, h), Square::new(w, h)];
-    let mut squares = vec![Square::new(w, h)];
+    let (view_w, view_h) = app.drawable_size();
+    let (model_w, model_h) = (view_h, view_w);
 
-    // Run the game loop
+    // Create some objects to live in the application
+    let mut squares = vec![
+        Square::new(model_w, model_h),
+        Square::new(model_w, model_h),
+        Square::new(model_w, model_h),
+    ];
+
+    // Run the main GUI loop
     let mut now = Instant::now();
+    eprintln!("Screen: {view_w}x{view_h}");
     while app.next_frame() {
         if now.elapsed() < Duration::from_millis(100) {
             continue;
@@ -166,27 +209,51 @@ fn main() {
         while app.has_event() {
             if let Event::Mouse {
                 event_type: MouseEventType::Down,
-                mouse_x,
-                mouse_y,
+                mouse_x: view_x,
+                mouse_y: view_y,
                 ..
             } = app.next_event()
             {
-                // If the user clicks, we add a new Square at the position of the mouse event.
+                // If the user clicks, we add a new Square at the
+                // position of the mouse event.
+
+                // Rotate mouse position from view to model
+                let (model_x, model_y) = Window::translate_view_90c(view_x, view_y, view_w, view_h);
                 squares.push(Square::new_at_position(
-                    mouse_x as f32,
-                    mouse_y as f32,
-                    w,
-                    h,
+                    model_x as f32,
+                    model_y as f32,
+                    model_w,
+                    model_h,
                 ));
             }
         }
 
-        app.clear();
+        app.clear_to_color(255, 255, 255);
 
         // update and draw
         for square in squares.iter_mut() {
             square.update();
             square.draw(&mut app);
+        }
+
+        // Draw brown rectangle on bottom of screen (max Y)
+        {
+            let bottom_rect = Rect::new(0, model_h as i32 - 10, model_w, 10);
+            // eprintln!("bottom_rect: {bottom_rect:?} w:{view_w} h:{view_h}");
+            fill_rect(&mut app, bottom_rect, [139, 69, 19, 127]);
+        }
+        // Draw cross
+        {
+            // vertical
+            let rect = Rect::new(model_w as i32 / 2 - 10, 0, 20, model_h);
+            eprintln!("Vertical:   {rect:?}  {model_w}x{model_h}");
+            fill_rect(&mut app, rect, [255, 0, 0, 127]);
+        }
+        {
+            // horizontal
+            let rect = Rect::new(0, model_h as i32 / 2 - 10, model_w, 20);
+            eprintln!("Horizontal: {rect:?}  {model_w}x{model_h}");
+            fill_rect(&mut app, rect, [255, 0, 0, 255]);
         }
     }
 }
